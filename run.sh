48bcff0b072790317a7f03b8fa4387845202931b
fiberleif <fiberleif@gmail.com>
2019-03-28 15:39:29 +0800

    3/28/2019: Add environment file and modify run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-#python scripts/im_pipeline.py pipelines/im_pipeline.yaml 0_sampletrajs
+python scripts/im_pipeline.py pipelines/im_pipeline.yaml 0_sampletrajs
 python scripts/im_pipeline.py pipelines/im_pipeline.yaml 1_train
