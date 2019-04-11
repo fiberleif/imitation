9fee31ce12391600c8c70ab1473471b9b86a90d3
fiberleif <fiberleif@gmail.com>
2019-04-11 06:53:43 +0000

    4/11/2019: Generate imperfect demos

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python scripts/im_pipeline.py pipelines/im_pipeline.yaml 0_sampletrajs
---
+++ run_humanoid.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python scripts/im_pipeline.py pipelines/im_humanoid_pipeline.yaml 0_sampletrajs
