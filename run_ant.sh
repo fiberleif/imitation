b9fdf72f81e5ebe882eda8a9dbcc53fca0d0df89
fiberleif <fiberleif@gmail.com>
2019-09-01 18:21:08 +0800

    Runnable.

---
+++ run_ant.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+CUDA_VISIBLE_DEVICES=0 python scripts/im_pipeline.py pipelines/im_ant_pipeline.yaml 0_sampletrajs
+CUDA_VISIBLE_DEVICES=0 python scripts/im_pipeline.py pipelines/im_ant_pipeline.yaml 1_train
+CUDA_VISIBLE_DEVICES=0 python scripts/im_pipeline.py pipelines/im_ant_pipeline.yaml 2_eval
