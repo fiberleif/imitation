c242a60d6eb08662d1c6a1bc20734f662ef5ae7f
fiberleif <fiberleif@gmail.com>
2019-09-05 21:56:03 +0800

    Modify pipeline files.

--- run_ant.sh
+++ run_ant.sh
@@ -1,2 +1,3 @@
 python scripts/im_pipeline.py pipelines/im_ant_pipeline.yaml 0_sampletrajs
 python scripts/im_pipeline.py pipelines/im_ant_pipeline.yaml 1_train
+python scripts/im_pipeline.py pipelines/im_ant_pipeline.yaml 2_eval
